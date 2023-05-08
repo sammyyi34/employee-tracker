2f7d633930a992364bc4cbea6e444230e7e8c2c4
Sammy <sammyhyi@gmail.com>
2023-05-08 17:47:20 -0400

    inserted data into table using seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES
+('Board of Directors'),
+('Engineering'),
+('Marketing'),
+('Sales'),
+('Human Resources');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CEO', 1000000.00, 1),
+('Senior Engineer' 175000.00, 2),
+('Marketing Manager', 150000.00, 3),
+('Sales Rep', 80000.00, 4),
+('HR Director', 120000.00, 5);
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES
+('LeBron', 'James', 1),
+('Drake', 'Graham', 2),
+('Kanye', 'West', 3),
+('Trae', 'Young', null),
+()
+       
