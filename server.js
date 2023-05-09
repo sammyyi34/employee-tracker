9f8004e12d91e31767ba43044cd35d7932802ce0
Sammy <sammyhyi@gmail.com>
2023-05-08 20:34:40 -0400

    created prompt question and switch cases for each prompt question

--- server.js
+++ server.js
@@ -2,9 +2,63 @@ const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
 require('console.table');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: 'localhost'
+});
+
+const questions = [
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'promptChoices',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department'
+    ]
+  }
+];
+
+function handleUserInput(answer) {
+  switch(answer.promptChoices) {
+    case 'View All Employees':
+      viewAllEmployees();
+      break;
+    case 'Add Employee':
+      addEmployee();
+      break;
+    case 'Update Employee Role':
+      updateEmployeeRole();
+      break;
+    case 'View All Roles':
+      viewAllRoles();
+      break;
+    case 'Add Role':
+      addRole();
+      break;
+    case 'View All Departments':
+      viewAllDepartments();
+      break;
+    case 'Add Department':
+      addDepartment();
+      break;
+    default:
+      console.log('Invalid choice. Please try again.');
+      break;
+  }
+};
